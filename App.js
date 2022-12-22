44c8445d232099a87fea4d8feff4aec83c241625
unknown <sarveshbhosale111@gmail.com>
2022-12-22 16:05:11 +0000

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Keyboard, KeyboardAvoidingView, Platform, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
+  const [task,setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+  const addTask = () =>{
+    setTaskItems([...taskItems,task])
+    setTask("")
+    Keyboard.dismiss();
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>All Events</Text>
+        <ScrollView style={styles.item}>
+          {
+            taskItems.map((item,index)=>{
+              return(<Task key={index} text={item}/>)
+            })
+          }
+        </ScrollView>
+      </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.writeTextWrapper}
+      >
+        <TextInput style={styles.input} placeholder={'Add Your Event'} value={task} onChangeText={e => setTask(e)}/>
+        <TouchableOpacity onPress={addTask}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View> 
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -13,8 +40,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  taskWrapper: {
+    paddingTop: 90,
+    paddingHorizontal: 20
+  },
+  sectionTitle: {
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  item: {
+    marginTop: 25,
+    maxHeight:"85%"
+  },
+  writeTextWrapper: {
+    position:'absolute',
+    bottom:40,
+    width:'100%',
+    flexDirection:'row',
+    justifyContent:'space-around',
+    alignItems:'center'
+  },
+  input: {
+    padding:15,
+    width:250,
+    backgroundColor:'#fff',
+    borderRadius:60,
+    borderColor:'#c0c0c0',
+    borderWidth:1,
+  },
+  addWrapper: {
+    width:60,
+    height:60,
+    backgroundColor:'#fff',
+    borderRadius:60,
+    borderColor:'#c0c0c0',
+    borderWidth:1,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  addText: {},
 });
---
+++ components/Task.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { View, Text, StyleSheet, Touchable, TouchableOpacity } from "react-native";
+
+const Task = ({ text }) => {
+    return (
+        <View style={styles.item}>
+            <View style={styles.itemLeft}>
+                <TouchableOpacity style={styles.square}></TouchableOpacity>
+                <View style={styles.itemTextContainer}>
+                    <Text style={styles.eventTitle}>{text}</Text>
+                    <Text style={styles.eventDescription}>{text}</Text>
+                </View>
+            </View>
+            <View style={styles.circular}>
+
+            </View>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: '#fff',
+        padding: 15,
+        borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: "center",
+        justifyContent: "space-between",
+        marginBottom: 15
+    },
+    itemLeft: {
+        flexDirection: "row",
+        alignItems: 'center',
+        flexWrap: 'wrap'
+    },
+    square: {
+        width: 24,
+        height: 24,
+        backgroundColor: '#55bcf6',
+        opacity: 0.4,
+        borderRadius: 5,
+        marginRight: 15
+    },
+    itemTextContainer: {
+        maxWidth: '80%'
+
+    },
+    circular: {
+        width: 12,
+        height: 12,
+        borderColor: '#55bcf6',
+        borderWidth: 2,
+        borderRadius: 5
+    },
+    eventTitle:{
+        fontWeight:"bold",
+        fontSize:17
+    }
+})
+export default Task;
